247ec29080c1520d88838f622117ff741a84228a
Kim Page <kpage989@gmail.com  git config --global user.name kimpage>
2019-03-06 06:26:24 +0700

    Initial Commit

---
+++ MockingBird/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MockingBird
+//
+//  Created by Kim Page on 3/6/19.
+//  Copyright © 2019 Kim Page. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
